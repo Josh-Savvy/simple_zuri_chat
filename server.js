b75b968d277c7b154e72d1f7576d6c7247608f8f
Josh Savvy <joshtee28@gmail.com>
2022-06-07 12:59:11 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.render("index");
 });
 
